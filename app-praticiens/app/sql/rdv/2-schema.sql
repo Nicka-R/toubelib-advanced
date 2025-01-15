b167a3af810febbd8ba7204299f1937a8bef7d32
Nicka-R <nicka.ratovobodo8@etu.univ-lorraine.fr>
2025-01-15 21:13:04 +0100

    ]refactor] remove unused SQL extensions and service exception classes

--- app-praticiens/app/sql/rdv/2-schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS "rdv";
-CREATE TABLE "public"."rdv" (
-    "id" uuid DEFAULT uuid_generate_v4() NOT NULL,
-    "date_heure" timestamp,
-    "patient_id" uuid,
-    "praticien_id" uuid,
-    CONSTRAINT "rdv_pkey" PRIMARY KEY ("id")
-) WITH (oids = false);
--- app-praticiens/app/sql/rdv/3-data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO "rdv" ("id", "date_heure", "patient_id", "praticien_id") VALUES
-('7e913093-e42f-4b08-813d-0ec0521a1301', '2025-01-17 16:00:00', '929b1929-1536-4752-8308-3e5fb85d6ad7', '2a8a9689-adbe-4859-84ac-a109e832339f'),
-('8f0241a2-b3c4-5d6e-7f89-0123456789ab', '2025-01-13 09:00:00', '2b3c4d5e-6f7a-8b9c-0d1e-2f3a4b5c6d7e', 'fdfe5f21-1e56-4d14-800d-d3c50c1d02af'),
-('5e6f7a8b-9c0d-4e1f-3a4b-5c6d7e8f9a0b', '2025-01-13 11:30:00', '3c4d5e6f-7a8b-9c0d-1e2f-3a4b5c6d7e8f', '4b5c6d7e-8f9a-4b0c-2d3e-4f5a6b7c8d9e');
