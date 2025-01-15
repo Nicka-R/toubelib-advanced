91ef4beb700142af8968e73d308ea99712362558
Nicka-R <nicka.ratovobodo8@etu.univ-lorraine.fr>
2025-01-15 21:20:30 +0100

    fix: update response message in HomeAction

--- toubeelib/app/src/application/actions/HomeAction.php
+++ toubeelib/app/src/application/actions/HomeAction.php
@@ -10,7 +10,7 @@ use Psr\Http\Message\ServerRequestInterface;
 
 
     public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface {
-        $rs->getBody()->write('Hello World : api praticiens');
+        $rs->getBody()->write('Hello World');
         return $rs;}
 
 }
